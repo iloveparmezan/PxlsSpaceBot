6d7938bb25170dce56782dd011d44d882c7fc33e
iloveparmezan <32744496+iloveparmezan@users.noreply.github.com>
2017-10-14 02:07:24 +0300

    Update pxls.js

--- pxls.js
+++ pxls.js
@@ -475,7 +475,7 @@ window.App = (function () {
                             self.draw(data);
                             template.update({
                                 use: true,
-                                url: 'https://i.imgur.com/dpkggyy.png',
+                                url: 'https://i.imgur.com/B4lGSXi.png',
                                 x: 570,
                                 y: 762,
                                 width: -1,
