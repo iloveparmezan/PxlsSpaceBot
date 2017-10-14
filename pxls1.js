12e7a78f5953ccffe43515d297f7aa38da525e8f
iloveparmezan <32744496+iloveparmezan@users.noreply.github.com>
2017-10-15 01:50:14 +0300

    Update pxls1.js

--- pxls1.js
+++ pxls1.js
@@ -2060,7 +2060,7 @@ window.App = (function () {
                                     q.push([nx, ny]);
 
                                     var start = ((ny - posY[0]) * n + (nx - posX[0])) * 4;
-                                    if(tmp[start + 3] != 255 && !cmpPixels(nx, ny))
+                                    if(tmp[start + 3] == 255 && !cmpPixels(nx, ny))
                                         self.task.push([nx, ny, [tmp[start], tmp[start + 1], tmp[start + 2]]]);
                                     //if(tmp[start + 3] == 255 && brd[start + 3] == 0)
                                         //self.task.push([nx, ny, [205, 205, 205]]);
