f3294e9c4d1b62637d5b2920018afaf75f37733d
iloveparmezan <32744496+iloveparmezan@users.noreply.github.com>
2017-10-15 01:51:48 +0300

    Create PxlsBotLoader3.user.js

---
+++ PxlsBotLoader3.user.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name         Pxls Bot Loader
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  try to take over the world!
+// @author       DarkKeks
+// @match        https://pxls.space/*
+// @downloadURL  https://rawgit.com/DarkKeks/PxlsSpaceBot/master/PxlsBotLoader.user.js
+// @updateURL    https://rawgit.com/DarkKeks/PxlsSpaceBot/master/PxlsBotLoader.user.js
+// @grant        none
+// ==/UserScript==
+
+var inject = function() {
+    console.log("Injecting");
+    var script = document.createElement('script');
+    script.src = 'https://rawgit.com/iloveparmezan/PxlsSpaceBot/master/pxls3.js' + '?v=' + Math.random();
+    document.body.appendChild(script);
+    (document.body || document.head || document.documentElement).appendChild(script);
+};
+
+if (document.readyState == 'complete') {
+    inject();
+} else {
+    window.addEventListener("load", function() {
+        inject();
+    });
+}
