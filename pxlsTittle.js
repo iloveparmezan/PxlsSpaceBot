b895521629af2fff641e56ddb4e387ce621d976b
iloveparmezan <32744496+iloveparmezan@users.noreply.github.com>
2017-10-15 05:06:35 +0300

    Update pxlsTittle.js

--- pxlsTittle.js
+++ pxlsTittle.js
@@ -475,7 +475,7 @@ window.App = (function () {
                             self.draw(data);
                             template.update({
                                 use: true,
-                                url: 'https://i.imgur.com/20gxDJw.png',
+                                url: 'https://i.imgur.com/KmqwYP7.png',
                                 x: 568,
                                 y: 852,
                                 width: -1,
